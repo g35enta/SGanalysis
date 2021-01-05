901a64509c53b1ac18cb902b174fdc844446aec4
g35enta <genta.ito@gmail.com>
2021-01-05 15:21:26 +0900

    Store segments in a SnapGene file into a SGSegment list

--- SGanalysis/Program.cs
+++ SGanalysis/Program.cs
@@ -4,74 +4,73 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace SGanalysis
 {
     class Program
     {
+        /// <summary>
+        /// SnapGeneファイルのセグメントを読み込むためのクラス
+        /// </summary>
+        public class SGSegment
+        {
+            public Byte SGIdentifier { get; set; }
+            public Int32 SGSize { get; set; } 
+            public Byte[] SGContent { get; set; }
+
+            public SGSegment(byte identifier, int size, byte[] content)
+            {
+                SGIdentifier = identifier;
+                SGSize = size;
+                SGContent = content;
+            }
+        }
+
+        /// <summary>
+        /// SnapGeneファイルからセグメント単位で読み込みます
+        /// </summary>
+        /// <param name="FileName">SnapGeneファイルパス</param>
+        /// <returns>全セグメントを格納したリスト</returns>
+        public static List<SGSegment> ReadSegments(string FileName)
+        {
+            // 全セグメントを格納するリスト
+            List<SGSegment> output = new List<SGSegment>();
+
+            using (FileStream fs = new FileStream(FileName, FileMode.Open, FileAccess.Read))
+            using (BinaryReader br = new BinaryReader(fs))
+            {
+                int fileSize = (int)fs.Length;               
+                while(br.BaseStream.Position != fileSize)
+                {
+                    byte id = br.ReadByte();
+                    byte[] sizeByteArray = br.ReadBytes(4);
+                    Array.Reverse(sizeByteArray);
+                    int size = BitConverter.ToInt32(sizeByteArray, 0);
+                    byte[] content = br.ReadBytes(size);
+                    SGSegment seg = new SGSegment(id, size, content);
+
+                    output.Add(seg);
+                }               
+            }
+
+            return output;
+        }
+
         static void Main(string[] args)
         {
             string[] filePath = Environment.GetCommandLineArgs();
             string dnaFilePath = filePath[1];
 
-            MyBytes dnaBytes = new MyBytes(dnaFilePath);
-        }
-    }
+            List<SGSegment> SGsegs = ReadSegments(dnaFilePath);
 
-    public class MyBytes
-    {
-        /// <summary>
-        /// バイナリを格納するバイト型リストを返すプロパティ
-        /// </summary>
-        private List<byte> Bytes { get; set; } = new List<byte>();
-
-        /// <summary>
-        /// 読み取り位置を返すプロパティ
-        /// </summary>
-        private int Pos { get; set; }
-
-        /// <summary>
-        /// 指定されたパスに存在するバイナリを読み込んでインスタンス化する
-        /// </summary>
-        /// <param name="filePath">バイナリファイルのパス</param>
-        public MyBytes(string filePath)
-        {
-            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-            using (BinaryReader br = new BinaryReader(fs))
+            foreach (SGSegment seg in SGsegs)
             {
-                Bytes.AddRange(br.ReadBytes((int)fs.Length));
-            }
-            // 初期位置は先頭
-            Pos = 0;
-        }
-
-        /// <summary>
-        ///現在位置の指定されたオフセット位置から指定されたバイト数だけ読み込む
-        /// </summary>
-        /// <param name="count">読み込むバイト数</param>
-        /// <param name="offsetPos">オフセットするバイト数</param>
-        /// <param name="readNext">現在位置を自動的に読み込み終了地点に進める場合にはtrue</param>
-        /// <returns></returns>
-        public byte[] ReadBytes(int count, int offsetPos = 0, bool readNext = true)
-        {
-            List<byte> result = Bytes.GetRange(Pos + offsetPos, count);
-            if (readNext)
-            {
-                Pos = Pos + offsetPos + count;
+                Console.WriteLine("ID  : {0}", seg.SGIdentifier);
+                Console.WriteLine("Size: {0}", seg.SGSize);
             }
 
-            return result.ToArray();
-        }
-
-        /// <summary>
-        /// ファイルの終端かどうかを返すプロパティ（読み取り専用）
-        /// </summary>
-        public bool EOF
-        {
-            get
-            {
-                return Bytes.Count <= Pos;
-            }
+            Console.ReadKey();
         }
     }
 }
