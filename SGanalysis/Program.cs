b7c7a3d4f6fa50b3c930f7588bf4e90e251b68ad
g35enta <g35enta@users.noreply.github.com>
2021-06-10 19:12:04 +0900

    210610 commit

--- SGanalysis/Program.cs
+++ SGanalysis/Program.cs
@@ -1,33 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections;
+using CsvHelper;
+using CsvHelper.Configuration;
 
 namespace SGanalysis
 {
     class Program
     {
-        /// <summary>
-        /// SnapGeneファイルのセグメントを読み込むためのクラス
-        /// </summary>
-        public class SGSegment
+        public interface ISGSegment
         {
-            public Byte SGIdentifier { get; set; }
-            public Int32 SGSize { get; set; } 
-            public Byte[] SGContent { get; set; }
-            public Byte SGSeqType { get; set; }
-            public Byte[] SGSeq { get; set; }
+            byte SGIdentifier { get; }
+            int SGSize { get; }
+            byte[] SGContent { get; }
+            byte SGSeqType { get; }
+            byte[] SGSeq { get; }
+        }
+
+        /// <summary>
+        /// SnapGeneファイルセグメントを1件保持する読み取り専用クラス
+        /// </summary>
+        public class SGSegment : ISGSegment
+        {
+            public Byte SGIdentifier { get; }
+            public Int32 SGSize { get; }
+            public Byte[] SGContent { get; }
+            public Byte SGSeqType { get; }
+            public Byte[] SGSeq { get; }
 
             public SGSegment() { }
-            public SGSegment(byte identifier, int size, byte[] content)
-            {
-                SGIdentifier = identifier;
-                SGSize = size;
-                SGContent = content;
-            }
             public SGSegment(byte identifier, int size, byte[] content, byte type, byte[] seq)
             {
                 SGIdentifier = identifier;
@@ -36,75 +37,111 @@ namespace SGanalysis
                 SGSeqType = type;
                 SGSeq = seq;
             }
-        }
-
-        /// <summary>
-        /// SnapGeneファイルからセグメント単位で読み込みます
-        /// </summary>
-        /// <param name="FileName">SnapGeneファイルパス</param>
-        /// <returns>全セグメントを格納したリスト</returns>
-        public static List<SGSegment> ReadSegments(string FileName)
-        {
-            // 全セグメントを格納するリスト
-            List<SGSegment> output = new List<SGSegment>();
-
-            using (FileStream fs = new FileStream(FileName, FileMode.Open, FileAccess.Read))
-            using (BinaryReader br = new BinaryReader(fs))
+            public SGSegment(byte identifier, int size, byte[] content)
             {
-                int fileSize = (int)fs.Length;               
-                while(br.BaseStream.Position != fileSize)
-                {
-                    byte id = br.ReadByte();
-                    byte[] sizeByteArray = br.ReadBytes(4);
-                    Array.Reverse(sizeByteArray);
-                    int size = BitConverter.ToInt32(sizeByteArray, 0);
-
-                    SGSegment seg = new SGSegment();
-
-                    if (id != 0)
-                    {
-                        byte[] content = br.ReadBytes(size);
-                        seg = new SGSegment(id, size, content);
-                    }
-                    else
-                    {
-                        byte[] content = br.ReadBytes(size);
-                        byte type = content[0];
-                        byte[] seq = new byte[content.Length - 1];
-                        Array.Copy(content, 1, seq, 0, content.Length - 1);
-                        seg = new SGSegment(id, size, content, type, seq);
-                    }
-
-                    output.Add(seg);
-                }               
+                SGIdentifier = identifier;
+                SGSize = size;
+                SGContent = content;
             }
-
-            return output;
         }
 
         /// <summary>
-        /// SnapGeneファイルを作成します
+        /// SnapGeneファイルを扱うための処理をサポートするインターフェース
         /// </summary>
-        /// <param name="SGsegs">SnapGeneセグメントを格納したリスト</param>
-        /// <param name="FileName">保存先のファイルパス</param>
-        /// <returns>ファイルが作成されたらtrueを返します</returns>
-        public static bool SaveSGFiles(List<SGSegment> SGsegs, string FileName)
+        public interface ISGFile
         {
-            if (SGsegs == null || FileName == "") return false;
+            // セグメントを列挙して返す
+            IEnumerable<SGSegment> Read();
+            // セグメントを受け取ってSGFileを作成する
+            bool Save(IEnumerable<SGSegment> segs, string filePath);
+        }
 
-            using (FileStream fs = new FileStream(FileName, FileMode.Create))
-            using (BinaryWriter bw = new BinaryWriter(fs))
+        /// <summary>
+        /// SnapGeneファイルを保持するクラス
+        /// </summary>
+        public class SGFile : ISGFile
+        {
+            // SnapGeneファイルのフルパス
+            private string filePath;
+            public SGFile() { }
+            public SGFile(string filePath)
             {
-                foreach (SGSegment seg in SGsegs)
+                this.filePath = filePath;
+            }
+            public IEnumerable<SGSegment> Read()
+            {
+                // 返す用のリスト
+                List<SGSegment> output = new List<SGSegment>();
+
+                using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                using (BinaryReader br = new BinaryReader(fs))
                 {
-                    bw.Write(seg.SGIdentifier);
-                    byte[] sizeByteArray = BitConverter.GetBytes(seg.SGSize); // LittleEndian
-                    Array.Reverse(sizeByteArray); // BigEndian
-                    bw.Write(sizeByteArray);
-                    bw.Write(seg.SGContent);
+                    int fileSize = (int)fs.Length;
+                    while (br.BaseStream.Position != fileSize)
+                    {
+                        SGSegment seg = new SGSegment();
+                        // id
+                        byte id = br.ReadByte();
+                        // size
+                        byte[] sizeByteArray = br.ReadBytes(4);
+                        Array.Reverse(sizeByteArray);
+                        int size = BitConverter.ToInt32(sizeByteArray, 0);
+                        // content
+                        byte[] content = br.ReadBytes(size);
+
+                        if (id == 0)
+                        {
+                            // contentの内部情報（type, seq）
+                            byte type = content[0];
+                            byte[] seq = new byte[content.Length - 1];
+                            Array.Copy(content, 1, seq, 0, content.Length - 1);
+                            //SGSegmentをインスタンス化
+                            seg = new SGSegment(id, size, content, type, seq);
+                        }
+                        else
+                        {
+                            seg = new SGSegment(id, size, content);
+                        }
+
+                        // List<SGSegment>に追加
+                        output.Add(seg);
+                    }
                 }
+
+                return output;
             }
-            return true;
+
+            public bool Save(IEnumerable<SGSegment> segs, string filePath)
+            {
+                if (segs == null || filePath == "") return false;
+
+                using (FileStream fs = new FileStream(filePath, FileMode.Create))
+                using (BinaryWriter bw = new BinaryWriter(fs))
+                {
+                    foreach (SGSegment seg in segs)
+                    {
+                        bw.Write(seg.SGIdentifier);
+                        byte[] sizeByteArray = BitConverter.GetBytes(seg.SGSize); // LittleEndian
+                        Array.Reverse(sizeByteArray); // BigEndian
+                        bw.Write(sizeByteArray);
+                        bw.Write(seg.SGContent);
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// ファイルパスから具象クラスをインスタンス化するFactoryクラス。
+        /// filePathで判別して処理を分岐可能。
+        /// </summary>
+        public static class ISGFileFactory
+        {
+            public static ISGFile Create(string filePath)
+            {
+                return new SGFile(filePath);
+            }
         }
 
         /// <summary>
@@ -159,80 +196,78 @@ namespace SGanalysis
             return output;
         }
 
+        /// <summary>
+        /// CSVファイル作成用
+        /// </summary>
+        internal class SGSegTable : ClassMap<SGSegment>
+        {
+            private SGSegTable()
+            {
+                Map(c => c.SGIdentifier).Index(0);
+                Map(c => c.SGSize).Index(1);
+                Map(c => c.SGContent).Index(2);
+                Map(c => c.SGSeqType).Index(3);
+                Map(c => c.SGSeq).Index(4);
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("======================================================");
-            Console.WriteLine("  SnapGeneファイル編集ツール");
+            Console.WriteLine("  SnapGeneファイル解析ツール");
             Console.WriteLine("    Copyright (c) 2021 Genta Ito");
-            Console.WriteLine("    Version 1.0");
+            Console.WriteLine("    Version 2.0");
             Console.WriteLine("======================================================");
+            Console.WriteLine("  ダンプファイルをテキストおよびCSVファイルで出力します。");
             Console.WriteLine("");
 
+            // ファイルパスの取得
             string[] filePath = Environment.GetCommandLineArgs();
             string dnaFilePath = filePath[1];
 
-            List<SGSegment> SGsegs = new List<SGSegment>();
-
-            try
-            {
-                SGsegs = ReadSegments(dnaFilePath);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("   エラーが発生しました。");
-                Console.WriteLine(ex.Message);
-                Console.ReadKey();
-                Environment.Exit(0);
-            }
-
-            if (SGsegs[0].SGIdentifier != 9)
-            {
-                Console.WriteLine("   SnapGeneファイルを読み込めません。");
-                Console.WriteLine("   ファイル形式を確認してください。");
-                Console.ReadKey();
-                Environment.Exit(0);
-            }
-
-            Console.WriteLine("   セグメント数: {0}", SGsegs.Count.ToString());
-
-            foreach (SGSegment seg in SGsegs)
-            {
-                if (seg.SGIdentifier != 0)
-                {
-                    Console.WriteLine("   ID     : {0}", seg.SGIdentifier.ToString());
-                    Console.WriteLine("   Size   : {0}", seg.SGSize.ToString());
-                    Console.WriteLine("   Content:");
-                    Console.WriteLine(System.Text.Encoding.UTF8.GetString(seg.SGContent));
-                    Console.WriteLine("");
-                }
-                else
-                {
-                    Console.WriteLine("   ID     : {0}", seg.SGIdentifier.ToString());
-                    Console.WriteLine("   Size   : {0}", seg.SGSize.ToString());
-                    Console.WriteLine("   Content:");
-                    Console.WriteLine(System.Text.Encoding.UTF8.GetString(seg.SGContent));
-                    Console.WriteLine("   Seq    :");
-                    Console.WriteLine(System.Text.Encoding.UTF8.GetString(seg.SGSeq));
-                    Console.WriteLine("   Type   : {0}", TypeString(seg.SGSeqType));
-                    Console.WriteLine("");
-                }
-            }
-
-            // セグメント#2を削除
-            int num = 0;
-            foreach (SGSegment seg in SGsegs)
-            {
-                if (seg.SGIdentifier == 2) break;
-                num++;
-            }
-            SGsegs.Remove(SGsegs[num]);
-
+            // ダンプ用ファイルパスの生成
             string dnaFileDirectory = Path.GetDirectoryName(dnaFilePath);
             string dnaFileName = Path.GetFileNameWithoutExtension(dnaFilePath);
-            string newFileName = dnaFileDirectory + "\\" + dnaFileName + "_" + ".dna";
-            SaveSGFiles(SGsegs, newFileName);
+            string dumpCsvFilePath = dnaFileDirectory + "\\" + dnaFileName + "_dump" + ".csv";
+            string dumpTxtFilePath = dnaFileDirectory + "\\" + dnaFileName + "_dump" + ".txt";
 
-            Console.WriteLine("   Saved {0}", newFileName);
+            // 読み込み
+            ISGFile file = ISGFileFactory.Create(dnaFilePath);
+            IEnumerable<SGSegment> SGsegs = file.Read();
+
+            // ダンプCSVファイル出力
+            using (var sw = new StreamWriter(dumpCsvFilePath))
+            using (var csv = new CsvWriter(sw, System.Globalization.CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(SGsegs);
+            }
+            Console.WriteLine("   Saved {0}", dumpCsvFilePath);
+            Console.WriteLine("");
+
+            // ダンプTXTファイル出力
+            using (var sw = new StreamWriter(dumpTxtFilePath, false))
+            {
+                foreach (SGSegment seg in SGsegs)
+                {
+                    sw.WriteLine("ID: " + seg.SGIdentifier.ToString());
+                    sw.WriteLine("Size: " + seg.SGSize.ToString());
+                    sw.WriteLine("Content:");
+                    sw.WriteLine(System.Text.Encoding.UTF8.GetString(seg.SGContent));
+                    if (seg.SGSeqType != 0) sw.WriteLine("Sequence Type: " + seg.SGSeqType.ToString());
+                    if (seg.SGSeq != null) sw.WriteLine(System.Text.Encoding.UTF8.GetString(seg.SGSeq));
+                    sw.WriteLine("");
+                }
+            }
+            Console.WriteLine("   Saved {0}", dumpTxtFilePath);
+            Console.WriteLine("");
+
+            // 仮実装
+            // 別のファイル名（末尾に"_"を追加）で保存
+            string newFilePath = dnaFileDirectory + "\\" + dnaFileName + "_" + ".dna";
+            file.Save(SGsegs, newFilePath);
+
+            Console.WriteLine("   Saved {0}", newFilePath);
+            Console.WriteLine("");
             Console.WriteLine("   Hit any keys to quit...");
             Console.ReadKey();
         }
